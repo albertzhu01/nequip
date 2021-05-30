ad703deb9654f0455046403a7b264aee8e8f1ef8
Albert Zhu <axzhu@college.harvard.edu>
2021-05-30 18:17:18 -0400

    plot features for individual atoms over epochs

--- nequip/scripts/training_features/plot_features_per_atom.py
+++ nequip/scripts/training_features/plot_features_per_atom.py
@@ -37,22 +37,12 @@ atomic_numbers = [6, 6, 6, 6, 6, 6, 6, 8, 8, 8, 6, 6, 8, 1, 1, 1, 1, 1, 1, 1, 1]
 # plt.show()
 
 # Code for creating line plots
-# avg_data_C1 = np.empty((1, 240))
-# epoch1_v = np.load('C:/Users/alber/nequip/nequip/scripts/training_features/feats_v_epoch10.npz')
-#
-# for key in epoch1_v.files:
-#     avg_data_C1 = np.concatenate((avg_data_C1, epoch1_v[key][0:105:21]))
-#
-# avg_data_C1 = avg_data_C1[1:]
-# avg_data_C1 = np.average(avg_data_C1, axis=1)
-# print(avg_data_C1)
-
 data_C1_all_epochs = []
 for i in 1, 5, 10:
     epoch_data = np.load('C:/Users/alber/nequip/nequip/scripts/training_features/feats_v_epoch' + str(i) + '.npz')
     tmp_data = np.empty((1, 240))
     for key in epoch_data.files:
-        tmp_data = np.concatenate((tmp_data, epoch_data[key][0:105:21]))
+        tmp_data = np.concatenate((tmp_data, epoch_data[key][11:105:21]))
 
     tmp_data = tmp_data[1:]
     avg_data_C1 = np.average(tmp_data, axis=0)
@@ -65,9 +55,9 @@ df_C1_all_epochs = pd.DataFrame(data_C1_all_epochs, index=['Epoch 1', 'Epoch 5',
 df_C1_all_epochs = df_C1_all_epochs.transpose()
 print(df_C1_all_epochs)
 
-df_C1_all_epochs.plot(kind='line', alpha=0.6)
+df_C1_all_epochs.plot(kind='line', alpha=0.6, ylim=(-0.45, 0.45))
 plt.legend(loc='lower right')
-plt.title('Carbon1 Training Validation Features (50-Atom Average)')
+plt.title('Carbon9 Training Validation Features (50-Atom Average)')
 plt.xlabel('Feature Index')
 plt.ylabel('Feature Value')
 plt.show()
