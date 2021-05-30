c62875f983d6e6f3625b29221f6db9402cdfcfcd
Albert Zhu <axzhu@college.harvard.edu>
2021-05-30 00:24:55 -0400

    plot several epochs for carbon1 features

--- nequip/scripts/training_features/plot_features_per_atom.py
+++ nequip/scripts/training_features/plot_features_per_atom.py
@@ -4,44 +4,67 @@ import pandas as pd
 import seaborn as sns
 
 atomic_numbers = [6, 6, 6, 6, 6, 6, 6, 8, 8, 8, 6, 6, 8, 1, 1, 1, 1, 1, 1, 1, 1]
-data_C1 = np.empty((1, 240))
-epoch1_v = np.load('C:/Users/alber/nequip/nequip/scripts/training_features/feats_v_epoch1.npz')
-# print(epoch1_v['batch1'][0:105:21])
-# print(data_C1)
-for key in epoch1_v.files:
-    data_C1 = np.concatenate((data_C1, epoch1_v[key][0:105:21]))
 
-data_C1 = data_C1[1:]
-print(data_C1.shape)
-
-stack_data = np.array([])
-for i in range(len(data_C1)):
-    stack_data = np.concatenate((stack_data, data_C1[i]))
-
-print(stack_data.size)
-
-index = np.array([])
-for i in range(50):
-    index = np.concatenate((index, np.arange(240)))
-
-index = index.tolist()
-
-df_C1 = pd.DataFrame(stack_data, index=index, columns=['Feature Value'])
-
-print(df_C1)
-
+# Code for creating 2d histograms per epoch
+# data_C1 = np.empty((1, 240))
+# epoch1_v = np.load('C:/Users/alber/nequip/nequip/scripts/training_features/feats_v_epoch10.npz')
 #
-# df_C1 = pd.DataFrame(data_C1)
-# df_C1['Index'] = range(240)
+# for key in epoch1_v.files:
+#     data_C1 = np.concatenate((data_C1, epoch1_v[key][0:105:21]))
 #
-# print(df_C1)
-
-sns.histplot(df_C1, x=df_C1.index, y='Feature Value', binwidth=(1, 0.004), cbar=True)
-
-plt.title('Carbon 1 Feature Vectors Epoch 1')
-plt.xlabel('Feature Vector Index')
-
-plt.show()
-
-# plt.hist2d(list[range(240)], data_C1, bins=40)
+# data_C1 = data_C1[1:]
+# print(data_C1.shape)
+#
+# stack_data = np.array([])
+# for i in range(len(data_C1)):
+#     stack_data = np.concatenate((stack_data, data_C1[i]))
+#
+# print(stack_data.size)
+#
+# index = np.array([])
+# for i in range(50):
+#     index = np.concatenate((index, np.arange(240)))
+#
+# index = index.tolist()
+#
+# df_C1 = pd.DataFrame(stack_data, index=index, columns=['Feature Value'])
+#
+# sns.histplot(df_C1, x=df_C1.index, y='Feature Value', binwidth=(1, 0.005), cbar=True)
+#
+# plt.title('Carbon 1 Feature Vectors Epoch 10')
+# plt.xlabel('Feature Vector Index')
+#
 # plt.show()
+
+# Code for creating line plots
+# avg_data_C1 = np.empty((1, 240))
+# epoch1_v = np.load('C:/Users/alber/nequip/nequip/scripts/training_features/feats_v_epoch10.npz')
+#
+# for key in epoch1_v.files:
+#     avg_data_C1 = np.concatenate((avg_data_C1, epoch1_v[key][0:105:21]))
+#
+# avg_data_C1 = avg_data_C1[1:]
+# avg_data_C1 = np.average(avg_data_C1, axis=1)
+# print(avg_data_C1)
+
+data_C1_all_epochs = []
+for i in 1, 3, 7, 10:
+    epoch_data = np.load('C:/Users/alber/nequip/nequip/scripts/training_features/feats_v_epoch' + str(i) + '.npz')
+    tmp_data = np.empty((1, 240))
+    for key in epoch_data.files:
+        tmp_data = np.concatenate((tmp_data, epoch_data[key][0:105:21]))
+
+    tmp_data = tmp_data[1:]
+    avg_data_C1 = np.average(tmp_data, axis=0)
+    data_C1_all_epochs.append(avg_data_C1)
+
+data_C1_all_epochs = np.array(data_C1_all_epochs)
+print(data_C1_all_epochs.shape)
+
+df_C1_all_epochs = pd.DataFrame(data_C1_all_epochs, index=(1, 3, 7, 10))
+df_C1_all_epochs = df_C1_all_epochs.transpose()
+print(df_C1_all_epochs)
+
+df_C1_all_epochs.plot(kind='line', alpha=0.6)
+plt.legend(loc='lower right')
+plt.show()
