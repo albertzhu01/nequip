736b4c5f0858102f6738a64d945dd37e55d57484
Albert Zhu <axzhu@college.harvard.edu>
2021-07-20 20:16:10 -0400

    code for plotting euc dists of best and worst 10 features

--- nequip/scripts/extract_features.py
+++ nequip/scripts/extract_features.py
@@ -136,7 +136,7 @@ for atom_idx in range(7):
     plt.figure()
     plt.subplots(figsize=(19, 9.5))
     sns.heatmap(df_euc_dists, mask=mask, square=True, cmap='YlGnBu')
-    plt.title('Euclidean Distance between Best 10 and Worst 10 Features of Carbon 1 (Based on Force MAE)')
+    plt.title('Euclidean Distance between Best and Worst 10 Features of Carbon 1 (Based on Force MAE)')
     plt.ylabel('Feature')
     plt.xlabel('Feature')
     plt.savefig("C1_bw_feature_dist.png")
