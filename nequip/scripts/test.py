d29aa104505bb2dce845dfca60c185d7879c39d1
Albert Zhu <axzhu@college.harvard.edu>
2021-10-25 22:47:00 -0400

    bpa ensembles

---
+++ nequip/scripts/test.py
@@ -0,0 +1,54 @@
+import torch
+import numpy as np
+from sklearn import mixture
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from ase.visualize import view
+from ase.io import read
+from sklearn.metrics import mean_absolute_error
+
+from nequip.utils import Config, dataset_from_config
+from nequip.data import AtomicDataDict, AtomicData, Collater
+from nequip.nn import SequentialGraphNetwork, SaveForOutput
+# matplotlib.rc('xtick', labelsize=20)
+# matplotlib.rc('ytick', labelsize=20)
+# test = np.random.rand(25)
+# print(test)
+# ind_smallest10 = np.argpartition(test, 10)[:10]
+# ind_largest10 = np.argpartition(test, -10)[-10:]
+# print(test[ind_smallest10])
+# print(test[ind_largest10])
+#
+# arr = np.array([1, 3, 2, 4, 5])
+# idx = arr.argsort()[-3:][::-1]
+# print(idx)
+# print(arr[idx])
+
+# atoms = read("C:/Users/alber/nequip/configs_final/test_300K.xyz")
+# for idx, atom in enumerate(atoms):
+#     print(atom)
+#     print(idx)
+# print(atoms)
+# view(atoms)
+
+# xyz = open("C:/Users/alber/nequip/configs_final/train_300K.xyz", "r")
+# for idx, line in enumerate(xyz):
+#     print(idx)
+#     print(line)
+# xyz.close()
+
+# greater = np.where(test > 0.5)
+# greater_elts = test[greater]
+# print(greater)
+# print(greater_elts)
+
+# plt.plot(np.arange(5, 10, 0.5), test[:10], label="test10")
+# plt.plot(test[10:])
+# plt.legend(["test"])
+# plt.show()
+
+perm = torch.randperm(500)
+print('[' + ', '.join(str(e.item()) for e in perm[:450]) + ']')
+print('[' + ', '.join(str(e.item()) for e in perm[450:500]) + ']')
