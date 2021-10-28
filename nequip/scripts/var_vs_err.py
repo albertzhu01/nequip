185811919625bb913fbdaf7525180c5ffdc7bddd
Albert Zhu <axzhu@college.harvard.edu>
2021-10-28 00:22:05 -0400

    first draft energy vars vs force mae

--- nequip/scripts/var_vs_err.py
+++ nequip/scripts/var_vs_err.py
@@ -16,7 +16,7 @@ train_force_maes = []
 test_pred_forces = []
 test_pred_energies = []
 test_force_maes = []
-for i in range(1):
+for i in range(10):
     train_pred_forces.append(np.load(f"/n/home10/axzhu/nequip/ensembles/train_forces_ensemble{i}.npz")['arr_0'])
     train_pred_energies.append(
         np.load(f"/n/home10/axzhu/nequip/ensembles/train_atomic_e_ensemble{i}.npz")['arr_0'].reshape(-1))
@@ -42,41 +42,48 @@ print(f"test_pred_forces shape: {test_pred_forces.shape}")
 print(f"test_pred_energies shape: {test_pred_energies.shape}")
 print(f"test_force_maes shape: {test_force_maes.shape}")
 
-# var_train_forces = np.var(train_pred_forces, axis=0)
-# var_train_energies = np.var(train_pred_energies, axis=0)
-# mean_train_maes = np.mean(train_force_maes, axis=0)
+var_train_forces = np.var(train_pred_forces, axis=0)
+var_train_energies = np.var(train_pred_energies, axis=0)
+mean_train_maes = np.mean(train_force_maes, axis=0)
 
-# var_test_forces = np.var(test_pred_forces, axis=0)
-# var_test_energies = np.var(test_pred_energies, axis=0)
-# mean_test_maes = np.mean(test_force_maes, axis=0)
+var_test_forces = np.var(test_pred_forces, axis=0)
+var_test_energies = np.var(test_pred_energies, axis=0)
+mean_test_maes = np.mean(test_force_maes, axis=0)
 
-# for i in range(27):
-#
-#     # Energy variance vs energy mae
-#     plt.figure()
-#     plt.subplots(figsize=(16, 9))
-#     plt.rc('xtick', labelsize=14)
-#     plt.rc('ytick', labelsize=14)
-#     plt.scatter(
-#         x=mean_train_maes[i:12150:27],
-#         y=var_train_energies[i:12150:27],
-#         color='k',
-#         label=f'Training Data'
-#     )
-#     plt.scatter(
-#         x=mean_test_maes[i:48735:27],
-#         y=var_test_energies[i:48735:27],
-#         color='b',
-#         label=f'Test Data'
-#     )
-#     plt.legend(fontsize=14)
-#     plt.title(
-#         f"Atom Index {i} Predicted Atomic Energy Variance vs. Force MAE (Train 300K, Test 300K)",
-#         fontsize=18
-#     )
-#     plt.xlabel("Force MAE (eV/A)", fontsize=16)
-#     plt.ylabel("Variance of Predicted Atomic Energies (eV^2)", fontsize=16)
-#     plt.savefig(f"atom{i}_e-var_vs_mae_300K.png")
+print(f"var_train_forces shape: {var_train_forces.shape}")
+print(f"var_train_energies shape: {var_train_energies.shape}")
+print(f"mean_train_maes shape: {mean_train_maes.shape}")
+print(f"var_test_forces shape: {var_test_forces.shape}")
+print(f"var_test_energies shape: {var_test_energies.shape}")
+print(f"mean_test_maes shape: {mean_test_maes.shape}")
+
+for i in range(27):
+
+    # Energy variance vs energy mae
+    plt.figure()
+    plt.subplots(figsize=(16, 9))
+    plt.rc('xtick', labelsize=14)
+    plt.rc('ytick', labelsize=14)
+    plt.scatter(
+        x=mean_train_maes[i:12150:27],
+        y=var_train_energies[i:12150:27],
+        color='k',
+        label=f'Training Data'
+    )
+    plt.scatter(
+        x=mean_test_maes[i:48735:27],
+        y=var_test_energies[i:48735:27],
+        color='b',
+        label=f'Test Data'
+    )
+    plt.legend(fontsize=14)
+    plt.title(
+        f"Atom Index {i} Predicted Atomic Energy Variance vs. Force MAE (Train 300K, Test 300K)",
+        fontsize=18
+    )
+    plt.xlabel("Force MAE (eV/A)", fontsize=16)
+    plt.ylabel("Variance of Predicted Atomic Energies (eV^2)", fontsize=16)
+    plt.savefig(f"atom{i}_e-var_vs_mae_300K.png")
 
     # Force variance vs. energy mae
     # plt.figure()
