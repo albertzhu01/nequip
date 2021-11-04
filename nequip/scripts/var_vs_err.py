12514e3702e122f4f193875574a9ead362e394e8
Albert Zhu <axzhu@college.harvard.edu>
2021-11-04 16:34:34 -0400

    total energy var vs atomic force var

--- nequip/scripts/var_vs_err.py
+++ nequip/scripts/var_vs_err.py
@@ -22,22 +22,22 @@ test_pred_forces = []
 test_pred_tot_e = []
 for i in range(10):
     train_pred_forces.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/train_forces_ensemble{i}_300K.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_600K/train_forces_ensemble{i}_600K.npz")['arr_0'])
     # train_pred_energies.append(
-    #     np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/train_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
+    #     np.load(f"/n/home10/axzhu/nequip/ensembles_600K/train_atomic_e_ensemble{i}_600K.npz")['arr_0'].reshape(-1))
     # train_force_maes.append(
-    #     np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/train_forces_mae_ensemble{i}_300K.npz")['arr_0'])
+    #     np.load(f"/n/home10/axzhu/nequip/ensembles_600K/train_forces_mae_ensemble{i}_600K.npz")['arr_0'])
     train_pred_tot_e.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/train_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
+        np.load(f"/n/home10/axzhu/nequip/ensembles_600K/train_tot_e_ensemble{i}_600K.npz")['arr_0'].reshape(-1))
 
     test_pred_forces.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/test_forces_ensemble{i}_300K.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_600K/test_forces_ensemble{i}_600K.npz")['arr_0'])
     # test_pred_energies.append(
-    #     np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/test_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
+    #     np.load(f"/n/home10/axzhu/nequip/ensembles_600K/test_atomic_e_ensemble{i}_600K.npz")['arr_0'].reshape(-1))
     # test_force_maes.append(
-    #     np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/test_forces_mae_ensemble{i}_300K.npz")['arr_0'])
+    #     np.load(f"/n/home10/axzhu/nequip/ensembles_600K/test_forces_mae_ensemble{i}_600K.npz")['arr_0'])
     test_pred_tot_e.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/test_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
+        np.load(f"/n/home10/axzhu/nequip/ensembles_600K/test_tot_e_ensemble{i}_600K.npz")['arr_0'].reshape(-1))
 
 train_pred_forces = np.array(train_pred_forces)
 # train_pred_energies = np.array(train_pred_energies)
@@ -102,12 +102,12 @@ plt.scatter(
 )
 plt.legend(fontsize=14)
 plt.title(
-    f"Max Atomic Force Variance vs. Total Energy Variance (Train 300K, Test 300K)",
+    f"Max Atomic Force Variance vs. Total Energy Variance (Train 300K, Test 600K)",
     fontsize=18
 )
 plt.xlabel("Total Energy Variance (eV^2)", fontsize=16)
 plt.ylabel("Max Atomic Force Variance ((eV/A)^2)", fontsize=16)
-plt.savefig(f"tot-e-var_vs_f-var_300K.png")
+plt.savefig(f"tot-e-var_vs_f-var_600K.png")
 
 # for i in range(27):
 #
