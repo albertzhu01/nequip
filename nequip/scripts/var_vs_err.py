1b37d6a9f60657799be6207ca23f034098e505c7
Albert Zhu <axzhu@college.harvard.edu>
2021-11-08 21:51:48 -0500

    cutoffs

--- nequip/scripts/var_vs_err.py
+++ nequip/scripts/var_vs_err.py
@@ -22,20 +22,20 @@ test_force_maes = []
 # test_pred_tot_e = []
 for i in range(10):
     train_pred_forces.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_600K/train_forces_ensemble{i}_600K.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_forces_ensemble{i}.npz")['arr_0'])
     # train_pred_energies.append(
     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
     train_force_maes.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_600K/train_forces_mae_ensemble{i}_600K.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_forces_mae_ensemble{i}.npz")['arr_0'])
     # train_pred_tot_e.append(
     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
 
     test_pred_forces.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_600K/test_forces_ensemble{i}_600K.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_forces_ensemble{i}.npz")['arr_0'])
     # test_pred_energies.append(
     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
     test_force_maes.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_600K/test_forces_mae_ensemble{i}_600K.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_forces_mae_ensemble{i}.npz")['arr_0'])
     # test_pred_tot_e.append(
     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
 
@@ -187,9 +187,9 @@ for i in range(27):
     plt.axvline(mae_cutoff, color='m', linestyle='--', label='Chemical accuracy cutoff')
     plt.legend(fontsize=16)
     plt.title(
-        f"Atom Index {i} Predicted Atomic Force Variance vs. Force MAE (Train 300K, Test 600K)",
+        f"Atom Index {i} Predicted Atomic Force Variance vs. Force MAE (Train 300K, Test 300K)",
         fontsize=24
     )
     plt.xlabel("Force MAE (eV/A)", fontsize=20)
     plt.ylabel("Variance of Predicted Atomic Forces ((eV/A)^2)", fontsize=20)
-    plt.savefig(f"atom{i}_f-var_vs_mae_600K.png")
+    plt.savefig(f"atom{i}_f-var_vs_mae_300K.png")
