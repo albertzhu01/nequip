d2294ef4f3559bb7e0917e4bae0da27eaa690dc9
Albert Zhu <axzhu@college.harvard.edu>
2022-01-30 21:02:52 -0500

    std dev for uncertainty

--- nequip/scripts/num_ensembles.py
+++ nequip/scripts/num_ensembles.py
@@ -20,41 +20,41 @@ test_pred_forces = []
 # test_pred_energies = []
 test_force_maes = []
 # test_pred_tot_e = []
-for i in range(10):
+# for i in range(10):
+#     train_pred_forces.append(
+#         np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_forces_ensemble{i}.npz")['arr_0'])
+#     # train_pred_energies.append(
+#     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
+#     train_force_maes.append(
+#         np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_forces_mae_ensemble{i}.npz")['arr_0'])
+#     # train_pred_tot_e.append(
+#     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
+#
+#     test_pred_forces.append(
+#         np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_forces_ensemble{i}.npz")['arr_0'])
+#     # test_pred_energies.append(
+#     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
+#     test_force_maes.append(
+#         np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_forces_mae_ensemble{i}.npz")['arr_0'])
+#     # test_pred_tot_e.append(
+#     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
+
+for i in range(20):
     train_pred_forces.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_forces_ensemble{i}.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/train_forces_ensemble{i}_200K.npz")['arr_0'])
     # train_pred_energies.append(
     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
     train_force_maes.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_forces_mae_ensemble{i}.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/train_forces_mae_ensemble{i}_200K.npz")['arr_0'])
     # train_pred_tot_e.append(
     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
 
     test_pred_forces.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_forces_ensemble{i}.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/test_forces_ensemble{i}_200K.npz")['arr_0'])
     # test_pred_energies.append(
     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
     test_force_maes.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_forces_mae_ensemble{i}.npz")['arr_0'])
-    # test_pred_tot_e.append(
-    #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
-
-for i in range(10, 20):
-    train_pred_forces.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_forces_ensemble{i}_300K.npz")['arr_0'])
-    # train_pred_energies.append(
-    #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
-    train_force_maes.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_forces_mae_ensemble{i}_300K.npz")['arr_0'])
-    # train_pred_tot_e.append(
-    #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/train_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
-
-    test_pred_forces.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_forces_ensemble{i}_300K.npz")['arr_0'])
-    # test_pred_energies.append(
-    #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_atomic_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
-    test_force_maes.append(
-        np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_forces_mae_ensemble{i}_300K.npz")['arr_0'])
+        np.load(f"/n/home10/axzhu/nequip/ensembles_1200K/test_forces_mae_ensemble{i}_200K.npz")['arr_0'])
     # test_pred_tot_e.append(
     #     np.load(f"/n/home10/axzhu/nequip/ensembles_300K/test_tot_e_ensemble{i}_300K.npz")['arr_0'].reshape(-1))
 
@@ -183,9 +183,9 @@ for i in range(num_bpa_atoms):
 
     plt.legend(fontsize=16)
     plt.title(
-        f"Atom Index {i} Atomic Force Stddev vs. Force MAE (Train 300K, Test 300K)",
+        f"Atom Index {i} Atomic Force Stddev vs. Force MAE (Train 300K, Test 1200K)",
         fontsize=24
     )
     plt.xlabel("Force MAE (eV/A)", fontsize=20)
     plt.ylabel("Standard Deviation of Predicted Atomic Forces (eV/A)", fontsize=20)
-    plt.savefig(f"atom{i}_num_ensem_stddev_300K.png")
+    plt.savefig(f"atom{i}_num_ensem_stddev_1200K.png")
