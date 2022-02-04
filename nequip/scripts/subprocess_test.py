66eb94e10d9935cb33ec7ad489ea3d88d413fe91
Albert Zhu <axzhu@college.harvard.edu>
2022-02-03 23:33:41 -0500

    subprocess for nequip-evaluate

--- nequip/scripts/subprocess_test.py
+++ nequip/scripts/subprocess_test.py
@@ -1,11 +1,11 @@
 import subprocess
 
-params = {"train-dir": " /n/home10/axzhu/nequip/results/bpa/minimal/ ",
+params = {" train-dir": " /n/home10/axzhu/nequip/results/bpa/minimal/ ",
           "output": " bpa-min-sub.xyz"}
 
 tmpdir = "/n/home10/axzhu/nequip/results/bpa/minimal/"
 retcode = subprocess.run(
-            ["nequip-evaluate "]
+            ["nequip-evaluate"]
             + sum(
                 (["--" + k, str(v)] for k, v in params.items() if v is not None),
                 [],
