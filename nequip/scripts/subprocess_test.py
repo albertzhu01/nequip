f0d619b61797bee145af891c737b9078fb632622
Albert Zhu <axzhu@college.harvard.edu>
2022-02-04 14:41:28 -0500

    0 epochs

--- nequip/scripts/subprocess_test.py
+++ nequip/scripts/subprocess_test.py
@@ -1,35 +1,16 @@
-# import subprocess
-#
-# params = {"train-dir": "/n/home10/axzhu/nequip/results/bpa/minimal/",
-#           "output": "bpasub.xyz"}
-#
-# tmpdir = "/n/home10/axzhu/nequip/"
-# retcode = subprocess.run(
-#             ["nequip-evaluate"]
-#             + sum(
-#                 (["--" + k, str(v)] for k, v in params.items() if v is not None),
-#                 [],
-#             ),
-#             cwd=tmpdir,
-#             stdout=subprocess.PIPE,
-#             stderr=subprocess.PIPE,
-#         )
-# retcode.check_returncode()
-#
-# # Check the output
-# metrics = dict(
-#     [
-#         tuple(e.strip() for e in line.split("=", 1))
-#         for line in retcode.stdout.decode().splitlines()
-#     ]
-# )
-# metrics = {k: float(v) for k, v in metrics.items()}
-
 import subprocess
+
+params = {"train-dir": "/n/home10/axzhu/nequip/results/bpa/minimal/",
+          "model": "/n/home10/axzhu/nequip/results/bpa/minimal_sub-no-train/last_model.pth",
+          "output": "bpasub.xyz"}
 
 tmpdir = "/n/home10/axzhu/nequip/"
 retcode = subprocess.run(
-            ["nequip-train", "/n/home10/axzhu/nequip/configs/minimal_bpa.yaml"],
+            ["nequip-evaluate"]
+            + sum(
+                (["--" + k, str(v)] for k, v in params.items() if v is not None),
+                [],
+            ),
             cwd=tmpdir,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -37,10 +18,21 @@ retcode = subprocess.run(
 retcode.check_returncode()
 
 # Check the output
-# metrics = dict(
-#     [
-#         tuple(e.strip() for e in line.split("=", 1))
-#         for line in retcode.stdout.decode().splitlines()
-#     ]
-# )
-# metrics = {k: float(v) for k, v in metrics.items()}
+metrics = dict(
+    [
+        tuple(e.strip() for e in line.split("=", 1))
+        for line in retcode.stdout.decode().splitlines()
+    ]
+)
+metrics = {k: float(v) for k, v in metrics.items()}
+
+# import subprocess
+#
+# tmpdir = "/n/home10/axzhu/nequip/"
+# retcode = subprocess.run(
+#             ["nequip-train", "/n/home10/axzhu/nequip/configs/minimal_bpa.yaml"],
+#             cwd=tmpdir,
+#             stdout=subprocess.PIPE,
+#             stderr=subprocess.PIPE,
+#         )
+# retcode.check_returncode()
